5ccfbd35d0cccd13f59d78553234b23b8faa4fb1
palassaha <123sahap640@gmail.com>
2024-11-14 00:54:19 +0530

    Question Added

--- Assignment-3/Ship.java
+++ Assignment-3/Ship.java
@@ -1,3 +1,28 @@
+// Design a Ship class that the following members:
+// ● A field for the name of the ship (a string).
+// ● A field for the year that the ship was built (a string).
+// ● A constructor and appropriate accessors and mutators.
+// ● A toString method that displays the ship’s name and the year it was
+// built.
+
+// Design a CruiseShip class that extends the Ship class. 
+// The CruiseShip class should have the following members:
+// ● A field for the maximum number of passengers (an int )
+// ● A constructor and appropriate accessors and mutators
+// ● A toString method that overrides the toString method in the base class.
+// The CruiseShip class’s toString method should display only the ship’s name and the maximum number of passengers.
+
+// Design a CargoShip class that extends the Ship class. 
+// The CargoShip class should have the following members:
+// ● A field for the cargo capacity in tonnage (an int )
+// ● A constructor and appropriate accessors and mutators
+// ● A toString method that overrides the toString method in the base class.
+// The CargoShip class’s toString method should display only the ship’s name and the ship’s cargo capacity.
+
+// Demonstrate the classes in a program that has a Ship array. 
+// Assign various Ship , CruiseShip , and CargoShip objects to the array elements. 
+// The program should then step through the array, calling each object’s toString method.
+
 public class Ship {
     public String name;
     public String yearBuilt;
