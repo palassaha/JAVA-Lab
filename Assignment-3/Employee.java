4124b35adb6dbcbbe864077d3bc1d6ed56abe314
palassaha <123sahap640@gmail.com>
2024-11-15 08:28:36 +0530

    files added with questions

---
+++ Assignment-3/Employee.java
@@ -0,0 +1,13 @@
+// In a particular factory a shift supervisor is a salaried employee who
+// supervises a shift. In addition to a salary, the shift supervisor earns a yearly
+// bonus when his or her shift meets production goals. Design a
+// ShiftSupervisor class that inherits from the Employee class. The
+// ShiftSupervisor class should have a field that holds the annual salary and a
+// field that holds the annual production bonus that a shift supervisor has
+// earned. Write one or more constructors and the appropriate accessor and
+// mutator methods for the class. Demonstrate the class by writing a program
+// that uses a ShiftSupervisor object.
+
+public class Employee {
+    
+}
---
+++ Assignment-3/Main2.java
@@ -0,0 +1,23 @@
+// Employee and ProductionWorker Classes: Design a class named
+// Employee. The class should keep the following information in fields:
+// i) Employee name
+// ii) Employee number in the format XXX-L, where each X is a
+// digit within the range 0-9 and the L is a letter within the range A-M.
+// iii) Hire date.
+// Write one or more constructors and the appropriate accessor and mutator
+// methods for the class.
+
+// Next, write a class named ProductionWorker that inherits from the
+// Employee class. The ProductionWorker class should have fields to hold the following information:
+// i) Shift (an integer)
+// ii) Hourly pay rate (a double)
+
+// The workday is divided into two shifts: day and night. The shift will be an
+// integer value representing the shift that the employee works. The day shift is
+// shift 1 and the night shift is shift 2. Write one or more constructors and the
+// appropriate accessor and mutator methods for the class. Demonstrate the
+// classes by writing a program that uses a ProductionWorker object.
+
+public class Main2 {
+    
+}
