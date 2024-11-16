0b7f4e76e2e9a8a29567d2c39930e251a8a75090
palassaha <123sahap640@gmail.com>
2024-11-16 08:52:03 +0530

    Assignment-2.9

--- Assignment-2/Figure.java
+++ Assignment-2/Figure.java
@@ -3,5 +3,50 @@
 // constructor overloading.
 
 public class Figure {
-    
+    private double side;
+    private double radius;
+    private double height;
+
+    // Constructor for Cube
+    public Figure(double side) {
+        this.side = side;
+    }
+
+    // Constructor for Cylinder
+    public Figure(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+
+    // Constructor for Sphere
+    public Figure(double radius, boolean isSphere) {
+        this.radius = radius;
+    }
+
+    public double surfaceAreaCube() {
+        return 6 * side * side;
+    }
+
+    public double surfaceAreaCylinder() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+
+    public double surfaceAreaSphere() {
+        return 4 * Math.PI * radius * radius;
+    }
+
+    public static void main(String[] args) {
+        // Cube
+        Figure cube = new Figure(5);
+        System.out.println("Surface area of cube: " + cube.surfaceAreaCube());
+
+        // Cylinder
+        Figure cylinder = new Figure(3, 7);
+        System.out.println("Surface area of cylinder: " + cylinder.surfaceAreaCylinder());
+
+        // Sphere
+        Figure sphere = new Figure(4, true);
+        System.out.println("Surface area of Sphere: " + sphere.surfaceAreaSphere());
+    }
 }
+
