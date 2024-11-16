a71bac475e3ba5539f0b1116b1739f73390d8f0b
palassaha <123sahap640@gmail.com>
2024-11-16 09:01:14 +0530

    Assignment-2.7

--- Assignment-2/Triangle.java
+++ Assignment-2/Triangle.java
@@ -10,5 +10,36 @@
 // highest area in main().
 
 public class Triangle {
-    
+    private int base;
+    private int height;
+
+    public Triangle(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double area() {
+        return 0.5 * base * height;
+    }
+
+    public void display() {
+        System.out.println("Base: " + base + ", Height: " + height + ", Area: " + area());
+    }
+
+    public static void main(String[] args) {
+        Triangle[] triangles = new Triangle[3];
+        triangles[0] = new Triangle(10, 5);
+        triangles[1] = new Triangle(8, 12);
+        triangles[2] = new Triangle(15, 7);
+
+        Triangle maxTriangle = triangles[0];
+        for (Triangle triangle : triangles) {
+            if (triangle.area() > maxTriangle.area()) {
+                maxTriangle = triangle;
+            }
+        }
+
+        System.out.print("Triangle with the maximum area: ");
+        maxTriangle.display();
+    }
 }
