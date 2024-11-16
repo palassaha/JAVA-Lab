604a7124d33546cedd09c75a58889204bd0869fa
palassaha <123sahap640@gmail.com>
2024-11-16 08:42:02 +0530

    Question added

---
+++ Assignment-2/Figure.java
@@ -0,0 +1,7 @@
+// Create a class named Figure. Make cube, cylinder and sphere as object of
+// the class Figure and calculate their surface area using concept of
+// constructor overloading.
+
+public class Figure {
+    
+}
---
+++ Assignment-2/Triangle.java
@@ -0,0 +1,14 @@
+// Create a class Triangle with two instance variables:
+// a. base: int
+// b. height: int
+// And two methods:
+// i. area(): double
+// ii. display(): void
+
+// Use constructor to initialized the instance variables. Create an array of
+// objects of type Triangle. Calculate and display the area of the object with
+// highest area in main().
+
+public class Triangle {
+    
+}
