14737d4b33054b24d5ee7369cce2b895b7fb0ade
palassaha <123sahap640@gmail.com>
2024-11-17 12:24:18 +0530

    Assignment-2.8

--- Assignment-2/Main8.java
+++ Assignment-2/Main8.java
@@ -1,3 +1,47 @@
+// Write a program using parameterized constructors to calculate area of a
+// square and a rectangle.
+
+class Square{
+    private int side;
+
+    public Square(int side){
+        this.side = side;
+    }
+
+    public int area(){
+        return side * side;
+    }
+
+    public void display(){
+        System.out.println("Square area: " + area());
+    }
+}
+
+class Rectangle{
+    private int length;
+    private int breadth;
+
+
+    public Rectangle(int length, int breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int area(){
+        return length * breadth;
+    }
+
+    public void display(){
+        System.out.println("Rectangle area: " + area());
+    }
+}
+
 public class Main8 {
-    
+    public static void main(String[] args) {
+        Square sq = new Square(5);
+        sq.display();
+
+        Rectangle rect = new Rectangle(10, 4);
+        rect.display();
+    }
 }
