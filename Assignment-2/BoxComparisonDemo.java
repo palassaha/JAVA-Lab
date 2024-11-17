8f7db8e9c8638d1256bc6382a4fda119fa15be82
palassaha <123sahap640@gmail.com>
2024-11-18 00:06:07 +0530

    questions

---
+++ Assignment-2/BoxComparisonDemo.java
@@ -0,0 +1,15 @@
+// Create a class Box with three instance variables:
+// a. width : int
+// b. height : int
+// c. depth : int
+// and three methods:
+// i. volume(): double
+// ii. display(): void
+// iii. compareVolume( Box, Box): Box
+
+// Create three instances of Box class and display the details of the object
+// with highest volume.
+
+public class BoxComparisonDemo {
+    
+}
---
+++ Assignment-2/PersonComparisonDemo.java
@@ -0,0 +1,7 @@
+// Create a class Person having name and age of a Person as data members.
+// Write a method that compare two persons by their age and print the
+// person’s name and age who is senior most.
+
+public class PersonComparisonDemo {
+    
+}
