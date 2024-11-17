c04a0f38fc8e440eee1c2d48b74743ffec75764b
palassaha <123sahap640@gmail.com>
2024-11-18 00:13:20 +0530

    Assignment-2.5

--- Assignment-2/PersonComparisonDemo.java
+++ Assignment-2/PersonComparisonDemo.java
@@ -2,6 +2,42 @@
 // Write a method that compare two persons by their age and print the
 // person’s name and age who is senior most.
 
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person compareAge(Person otherPerson) {
+        return (this.age > otherPerson.age) ? this : otherPerson;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+
 public class PersonComparisonDemo {
-    
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Alice", 30);
+        Person person2 = new Person("Bob", 45);
+
+        Person seniorMost = person1.compareAge(person2);
+
+        System.out.println("The senior-most person is:");
+        seniorMost.display();
+    }
 }
