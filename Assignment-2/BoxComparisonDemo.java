bbe289fd69d0d29812d1fe2ea891d1de203e8745
palassaha <123sahap640@gmail.com>
2024-11-18 00:08:42 +0530

    Assignment-2.4

--- Assignment-2/BoxComparisonDemo.java
+++ Assignment-2/BoxComparisonDemo.java
@@ -10,6 +10,41 @@
 // Create three instances of Box class and display the details of the object
 // with highest volume.
 
+class Box {
+    private int width;
+    private int height;
+    private int depth;
+
+    public Box(int width, int height, int depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public double volume() {
+        return width * height * depth;
+    }
+
+    public void display() {
+        System.out.println("Box Dimensions (Width x Height x Depth): " + width + " x " + height + " x " + depth);
+        System.out.println("Volume: " + volume());
+    }
+
+    public Box compareVolume(Box otherBox) {
+        return (this.volume() > otherBox.volume()) ? this : otherBox;
+    }
+}
+
 public class BoxComparisonDemo {
-    
+    public static void main(String[] args) {
+
+        Box box1 = new Box(3, 4, 5); // Volume: 60
+        Box box2 = new Box(6, 7, 8); // Volume: 336
+        Box box3 = new Box(2, 2, 2); // Volume: 8
+
+        Box largestBox = box1.compareVolume(box2).compareVolume(box3);
+
+        System.out.println("The box with the highest volume is:");
+        largestBox.display();
+    }
 }
