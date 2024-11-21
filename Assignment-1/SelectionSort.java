3168386333550ffa97d06f29bc76870499766731
palassaha <123sahap640@gmail.com>
2024-11-21 19:15:14 +0530

    Assignment-1.12

--- Assignment-1/SelectionSort.java
+++ Assignment-1/SelectionSort.java
@@ -1,1 +1,40 @@
 // Write a Java program to implement Selection sort.
+
+public class SelectionSort {
+    public void selectionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        SelectionSort sorter = new SelectionSort();
+
+        System.out.println("Unsorted Array:");
+        sorter.printArray(array);
+
+        sorter.selectionSort(array);
+
+        System.out.println("Sorted Array:");
+        sorter.printArray(array);
+    }
+}
