4c95c8e2f2aeca5db96bb330f32cb2cfdbe61ef4
palassaha <123sahap640@gmail.com>
2024-11-21 19:07:00 +0530

    Assignment-1.10

--- Assignment-1/StringToInteger.java
+++ Assignment-1/StringToInteger.java
@@ -1,5 +1,19 @@
 // Write a Java Program to convert a string to an integer in Java
 
+import java.util.Scanner;
+
 public class StringToInteger {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number as a string: ");
+        String input = scanner.nextLine();
+
+        try {
+            int number = Integer.parseInt(input);
+            System.out.println("The integer value is: " + number);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input! Please enter a valid numeric string.");
+        }
+        scanner.close();
+    }
 }
