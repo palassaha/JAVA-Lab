49317dc582125fce4c69e816c5b5bf71396588a8
palassaha <123sahap640@gmail.com>
2024-11-22 12:53:37 +0530

    Assignment-1.7

--- Assignment-1/Calculator.java
+++ Assignment-1/Calculator.java
@@ -1,5 +1,57 @@
-// Write a Java program to design a calculator using switch case.
+// Write a Java program to design a calculator using switch case.import java.util.Scanner;
+
+import java.util.Scanner;
 
 public class Calculator {
-    
+    public void calculate() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Calculator!");
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator. Please use +, -, *, or /.");
+        }
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.calculate();
+    }
 }
