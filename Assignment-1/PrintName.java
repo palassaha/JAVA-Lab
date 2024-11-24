256817fa7dca9ff9a9de211de3e8ef88cd9192af
palassaha <123sahap640@gmail.com>
2024-11-25 00:17:33 +0530

    Assignment-1.1

---
+++ Assignment-1/PrintName.java
@@ -0,0 +1,7 @@
+// Write a Java Program to print your name.
+
+public class PrintName {
+    public static void main(String[] args) {
+        System.out.println("Dibakorx Bonnerjee. Yayyy");
+    }
+}
