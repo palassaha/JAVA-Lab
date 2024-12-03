c811c28d2778f4a1ba1cc42958219ce8c276ac43
palassaha <123sahap640@gmail.com>
2024-12-03 12:15:16 +0530

    Assignment-5.3

--- Assignment-5/DoubleToString.java
+++ Assignment-5/DoubleToString.java
@@ -1,5 +1,11 @@
 // Write a Java program to convert double value to String.
 
-public class DoubleToString  {
-    
+public class DoubleToString {
+    public static void main(String[] args) {
+        double doubleValue = 123.456;
+        String stringValue = String.valueOf(doubleValue);
+        System.out.println("The double value as a String is: " + stringValue);
+        String stringValueAlternative = Double.toString(doubleValue);
+        System.out.println("The double value as a String (alternative) is: " + stringValueAlternative);
+    }
 }
