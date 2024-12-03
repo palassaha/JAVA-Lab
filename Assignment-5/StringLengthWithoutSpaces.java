643d7d00dfcf5c2aa542c85b87ca5cc3a94c5a57
palassaha <123sahap640@gmail.com>
2024-12-03 12:17:33 +0530

    Assignment-5.2

--- Assignment-5/StringLengthWithoutSpaces.java
+++ Assignment-5/StringLengthWithoutSpaces.java
@@ -1,5 +1,20 @@
 // Write a Java program to find the length of a String except white spaces.
 
+import java.util.Scanner;
+
 public class StringLengthWithoutSpaces {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+
+        String stringWithoutSpaces = input.replaceAll("\\s", "");
+
+        int length = stringWithoutSpaces.length();
+
+        System.out.println("The length of the string excluding white spaces is: " + length);
+
+        scanner.close();
+    }
 }
